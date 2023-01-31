4c7393d6aa9294504abe7111622a2e9121b74917
camila7866 <caveo7u7@gmail.com>
2023-01-31 17:54:10 -0600

    primeras prubas con motion magic

--- src/main/java/frc/robot/commands/ExampleCommand.java
+++ src/main/java/frc/robot/commands/ExampleCommand.java
@@ -1,7 +1,3 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.commands;
 
 import frc.robot.subsystems.ExampleSubsystem;
@@ -20,16 +16,21 @@ public class ExampleCommand extends CommandBase {
   public ExampleCommand(ExampleSubsystem subsystem) {
     m_subsystem = subsystem;
     // Use addRequirements() here to declare subsystem dependencies.
+
     addRequirements(subsystem);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    m_subsystem.talon.setSelectedSensorPosition(0);
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    m_subsystem.RunPosition();
+  }
 
   // Called once the command ends or is interrupted.
   @Override
--- src/main/java/frc/robot/subsystems/ExampleSubsystem.java
+++ src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -4,12 +4,37 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.StatusFrameEnhanced;
+import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
+import com.ctre.phoenix.motorcontrol.TalonFXFeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ExampleSubsystem extends SubsystemBase {
+  public static TalonFX talon = new TalonFX(0);
   /** Creates a new ExampleSubsystem. */
-  public ExampleSubsystem() {}
+  public ExampleSubsystem() {
+    talon.configFactoryDefault();
+    talon.configSelectedFeedbackSensor(TalonFXFeedbackDevice.IntegratedSensor, 0, 0);
+    talon.configNeutralDeadband(0.001);
+    talon.setSensorPhase(false);
+    talon.setStatusFramePeriod(StatusFrameEnhanced.Status_13_Base_PIDF0, 10, 0);
+		talon.setStatusFramePeriod(StatusFrameEnhanced.Status_10_MotionMagic, 10, 0);
+    talon.configNominalOutputForward(0);
+		talon.configNominalOutputReverse(0);
+		talon.configPeakOutputForward(1);
+		talon.configPeakOutputReverse(-1); 
+    talon.selectProfileSlot(0, 0);
+    talon.config_kF(0, 0.2);
+		talon.config_kP(0, 0.2);
+		talon.config_kI(0, 0);
+	  talon.config_kD(0, 0);
+    talon.configMotionCruiseVelocity(15000);
+    talon.configMotionAcceleration(6000);
+    talon.setSelectedSensorPosition(0);
+  }
 
   /**
    * Example command factory method.
@@ -33,6 +58,11 @@ public class ExampleSubsystem extends SubsystemBase {
   public boolean exampleCondition() {
     // Query some boolean state, such as a digital sensor.
     return false;
+  }
+
+  public void RunPosition (){
+    double targetPos = 0.5 * 2048 * 5;
+		talon.set(TalonFXControlMode.MotionMagic, targetPos);
   }
 
   @Override
