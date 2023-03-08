1a71e3b1b87ea99d3d244acd619a6c4ce754850e
Noel Rincón <72935460+NOEL318@users.noreply.github.com>
2023-03-07 19:12:42 -0600

    aaa

---
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -0,0 +1,11 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Limelight extends SubsystemBase {
+  public Limelight() {}
+
+  @Override
+  public void periodic() {
+  }
+}
